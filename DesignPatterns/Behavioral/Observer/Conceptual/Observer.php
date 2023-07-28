f10e3c3c56b1aedc4aac4a95e53b4d729a44b97a
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2023-07-28 22:01:23 +0400

    Update Observer.php

--- DesignPatterns/Behavioral/Observer/Conceptual/Observer.php
+++ DesignPatterns/Behavioral/Observer/Conceptual/Observer.php
@@ -48,7 +48,7 @@ class Observer implements \SplSubject
     /**
      * Usually, the subscription logic is only a fraction of what a Subject can
      * really do. Subjects commonly hold some imp business logic, that
-     * triggers a notification method whenever something important is about to
+     * triggers a notification method whenever something impt is about to
      * happen (or after it).
      */
     public function someBusinessLogic(): void
